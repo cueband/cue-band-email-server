76954874b1972cb7657fa5b468de77939e2f5396
Luis Carvalho <lapc19@hotmail.com>
2021-11-02 16:44:05 +0000

    Cleaning

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ var logger = require('morgan');
 require('dotenv').config()
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 var signupRouter = require('./routes/signup');
 
 
@@ -22,13 +21,12 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+//app.use('/', indexRouter);
 app.use('/signup', signupRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+  res.status(404).send();
 });
 
 // error handler
